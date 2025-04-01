ee9c23a2e31f39618dd1a3aa2bca1f87459027af
DavideGiuseppeCiliento <cilientodg@gmail.com>
2025-04-01 16:11:12 +0200

    Ho completato l'algoritmo e inserito i controlli isNAN e <= 0 per le cifre inserite.

--- js/script.js
+++ js/script.js
@@ -8,36 +8,38 @@
 // - L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare
 //   centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
-// INPUT
+// ----------------------------------------- INPUT
 
 const kmPrice = 0.21;
 
-const kmQuantity = prompt("Quanti Km dovrai percorrere?");
-const userAge = prompt("Quanti anni hai?");
+const kmQuantity = parseInt(prompt("Quanti Km dovrai percorrere?"));
+const userAge = parseInt(prompt("Quanti anni hai?"));
 const discountPriceOver65 = 40;
 const discountPriceUnder18 = 20;
 
-// ALGORITMO
-
 let totalPrice = kmQuantity * kmPrice;
 
-if (userAge > 18 && userAge < 65) {
+// ----------------------------------------- VALIDAZIONE
+if (isNaN(kmQuantity) || kmQuantity <= 0 || isNaN(userAge) || userAge <= 0) {
+  alert("!Inserisci valori validi: i chilometri e l'età devono essere numeri maggiori di 0!");
+}
+// BIGLIETTO PREZZO INTERO
+else if (userAge > 18 && userAge < 65) {
   alert("Il costo del tuo biglietto è di € " + totalPrice);
 }
 // SCONTO SE MINORENNI
 else if (userAge < 18) {
   let discountUnder18 = (totalPrice * discountPriceUnder18) / 100;
   totalPrice = totalPrice - discountUnder18;
-  alert("Il costo del tuo biglietto è di € " + totalPrice + "Con uno sconto di € " + discountUnder18);
+  alert("Il costo del tuo biglietto è di € " + totalPrice.toFixed(2) + " Con uno sconto di € " + discountUnder18.toFixed(2));
 }
 // SCONTO SE ANZIANI
 else if (userAge > 65) {
   let discountOver65 = (totalPrice * discountPriceOver65) / 100;
   totalPrice = totalPrice - discountOver65;
+  alert("Il costo del tuo biglietto è di € " + totalPrice.toFixed(2) + " Con uno sconto di € " + discountOver65.toFixed(2));
 }
 // GESTIONE ERRORI
 else {
   alert("Errore generico, ricarica la pagina");
 }
-
-// OUTPUT
